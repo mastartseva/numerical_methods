3d7fa9ebfa833984871c19cddbb677f5835d4e66
mastartseva <109406043+mastartseva@users.noreply.github.com>
2023-12-02 14:36:38 +0300

    Add files via upload

---
+++ lab_1/лаба 1.1.py
@@ -0,0 +1,25 @@
+def function(x):
+    return (x - 2) * ((x - 1) ** 3) * ((x - 3) ** 2)
+def dih(a,b,max_iter):
+    e=0.01
+    iter_count=0
+    if function(a)*function(b)<0 and iter_count<max_iter:
+        while abs(b-a)>e:
+            x0=(a+b)/2
+            if function(x0)==0:
+                return x0
+            elif function(a)*function(x0)<0:
+                b=x0
+            else:
+                a=x0
+            iter_count+=1
+        return x0       
+    else:
+        print("Нет корней")
+
+a,b=map(float,input().split())    
+print(dih(a,b,20))
+
+
+        
+
